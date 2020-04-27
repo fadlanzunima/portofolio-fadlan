7fd93751488b902e02bd2367901ba0e8332ee71d
fadlan.yamisok <fadlan.zunima@yamisok.com>
2020-04-27 20:54:16 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,58 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <ul>
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/about">About</router-link>
+        </li>
+      </ul>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
 #app {
+  padding: 0;
+  margin: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fbfafb;
 }
 
 #nav {
-  padding: 30px;
+  width: 100%;
+  height: 6rem;
+  background: #b0acbe;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #fbfafb;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #486789;
+    }
+  }
+
+  ul {
+    display: flex;
+    justify-content: flex-end;
+    margin: 0;
+    padding: 2rem;
+
+    li {
+      margin-left: 30px;
+      list-style: none;
+      font-size: 1.5rem;
+
+      a {
+        text-decoration: none;
+      }
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="img-user">
+      <img
+        src="https://media-exp1.licdn.com/dms/image/C5103AQG7PA4Sq3hnVA/profile-displayphoto-shrink_200_200/0?e=1593648000&v=beta&t=QrvXyqp1Hx-Aqz6u0Lf6OJFlNG2FRjpSVDly2euD5wA"
+        alt="img"
+      />
+    </div>
+    <div class="txt-wrapper">
+      <h1>Fadlan Zunima</h1>
+      <h3>Frontend Developer</h3>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    // HelloWorld
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 49vw;
+  background: #f5d7c4;
+  color: #2b2734;
+
+  .img-user {
+    width: 15rem;
+    height: fit-content;
+    border-radius: 50%;
+    img {
+      border-radius: 50%;
+    }
+  }
+
+  .txt-wrapper {
+    text-align: left;
   }
 }
-</script>
+</style>
